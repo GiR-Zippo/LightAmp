bd022a6eb67b648e752204723a43f6aec7f07369
GiR_Zippo <gir_zippo@hellokitty.com>
2022-06-02 15:54:39 +0200

    [BMPSong] set from 50 to30ms

--- BardMusicPlayer.Transmogrify/Song/BmpSong.cs
+++ BardMusicPlayer.Transmogrify/Song/BmpSong.cs
@@ -350,9 +350,9 @@ namespace BardMusicPlayer.Transmogrify.Song
                             long lastOn = notesToFix[k].GetTimedNoteOnEvent().Time;
                             if (lastOn < lowestParent) lowestParent = lastOn;
                         }
-                        if (lowestParent <= time + 50)
+                        if (lowestParent <= time + 30)
                         {
-                            time = lowestParent - 50;
+                            time = lowestParent - 30;
                             if (time < 0) continue;
                             notesToFix[i].Time = time;
                             dur = 25;
