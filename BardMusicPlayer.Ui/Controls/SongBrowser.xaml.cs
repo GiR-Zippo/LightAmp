013da5314a14f4eec50649b7e49a27e1bad0adcc
GiR_Zippo <gir_zippo@hellokitty.com>
2022-06-09 21:27:43 +0200

    Classic Ui] and the crash fixes for the songbrowser

--- BardMusicPlayer.Ui/Controls/SongBrowser.xaml.cs
+++ BardMusicPlayer.Ui/Controls/SongBrowser.xaml.cs
@@ -23,6 +23,9 @@ namespace BardMusicPlayer.Ui.Controls
         public SongBrowser()
         {
             InitializeComponent();
+            if (Directory.Exists(SongPath.Text))
+                return;
+
             string[] files = Directory.GetFiles( SongPath.Text, "*", SearchOption.AllDirectories);
             List<string> list = new List<string>(files);
             SongbrowserContainer.ItemsSource = list;
@@ -31,12 +34,17 @@ namespace BardMusicPlayer.Ui.Controls
         private void SongbrowserContainer_PreviewMouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             string filename = SongbrowserContainer.SelectedItem as String;
-            Console.WriteLine(filename);
+            if (File.Exists(filename) || filename == null)
+                return;
+
             PlaybackFunctions.LoadSong(filename);
         }
 
         private void SongSearch_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
         {
+            if (Directory.Exists(SongPath.Text))
+                return;
+
             string[] files = Directory.GetFiles(SongPath.Text, "*", SearchOption.AllDirectories);
             List<string> list = new List<string>(files);
             if (SongSearch.Text != "")
