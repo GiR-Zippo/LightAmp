80f8c4dc2f150c299f2e80992690d59df76939b6
GiR_Zippo <gir_zippo@hellokitty.com>
2022-07-05 21:14:34 +0200

    Fix for xiv 6.18 patch

--- BardMusicPlayer.Seer/Reader/Backend/Machina/MachinaReaderBackend.cs
+++ BardMusicPlayer.Seer/Reader/Backend/Machina/MachinaReaderBackend.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright(c) 2021 MoogleTroupe
  * Licensed under the GPL v3 license. See https://github.com/BardMusicPlayer/BardMusicPlayer/blob/develop/LICENSE for full license information.
  */
