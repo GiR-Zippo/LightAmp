031c9800eb1d4ecef3a9cf721901feeddbe0f444
GiR_Zippo <gir_zippo@hellokitty.com>
2022-08-08 14:51:19 +0200

    [Seer] Update Copyright date

--- BardMusicPlayer.Seer/Events/LatencyUpdate.cs
+++ BardMusicPlayer.Seer/Events/LatencyUpdate.cs
@@ -1,4 +1,9 @@
-﻿namespace BardMusicPlayer.Seer.Events
+﻿/*
+ * Copyright(c) 2022 GiR-Zippo
+ * Licensed under the GPL v3 license. See https://github.com/GiR-Zippo/LightAmp/blob/main/LICENSE for full license information.
+*/
+
+namespace BardMusicPlayer.Seer.Events
 {
     public sealed class LatencyUpdate : SeerEvent
     {
