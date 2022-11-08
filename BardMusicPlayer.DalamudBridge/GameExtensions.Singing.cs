5771fec21caf9f13841ddff82e8b0eb914a2f6de
GiR_Zippo <gir_zippo@hellokitty.com>
2022-11-08 02:00:38 +0100

    and one was missing

--- BardMusicPlayer.DalamudBridge/GameExtensions.Singing.cs
+++ BardMusicPlayer.DalamudBridge/GameExtensions.Singing.cs
@@ -11,7 +11,7 @@ namespace BardMusicPlayer.DalamudBridge
         private static readonly SemaphoreSlim LyricSemaphoreSlim = new (1,1);
 
         /// <summary>
-        /// Sands a lyric line via say
+        /// Sends a lyric line via say
         /// </summary>
         /// <param name="game"></param>
         /// <param name="text"></param>
@@ -25,6 +25,13 @@ namespace BardMusicPlayer.DalamudBridge
             return Task.FromResult(false);
         }
 
+        /// <summary>
+        /// sends a text in chat without interrupting playback
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="type"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public static Task<bool> SendText(this Game game, ChatMessageChannelType type, string text)
         {
             if (!DalamudBridge.Instance.Started) throw new DalamudBridgeException("DalamudBridge not started.");
@@ -34,6 +41,12 @@ namespace BardMusicPlayer.DalamudBridge
             return Task.FromResult(false);
         }
 
+        /// <summary>
+        /// Open or close an instrument
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="instrumentID"></param>
+        /// <returns></returns>
         public static Task<bool> OpenInstrument(this Game game, int instrumentID)
         {
             if (!DalamudBridge.Instance.Started) throw new DalamudBridgeException("DalamudBridge not started.");
@@ -43,12 +56,33 @@ namespace BardMusicPlayer.DalamudBridge
             return Task.FromResult(false);
         }
 
+        /// <summary>
+        /// Accept the ens request
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="arg"></param>
+        /// <returns></returns>
         public static Task<bool> AcceptEnsemble(this Game game, bool arg)
         {
             if (!DalamudBridge.Instance.Started) throw new DalamudBridgeException("DalamudBridge not started.");
 
             if (DalamudBridge.Instance.DalamudServer.IsConnected(game.Pid))
                 return Task.FromResult(DalamudBridge.Instance.DalamudServer.SendAcceptEnsemble(game.Pid, arg));
+            return Task.FromResult(false);
+        }
+
+        /// <summary>
+        /// Sets the objects to low or max
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="low"></param>
+        /// <returns></returns>
+        public static Task<bool> GfxSetLow(this Game game, bool low)
+        {
+            if (!DalamudBridge.Instance.Started) throw new DalamudBridgeException("DalamudBridge not started.");
+
+            if (DalamudBridge.Instance.DalamudServer.IsConnected(game.Pid))
+                return Task.FromResult(DalamudBridge.Instance.DalamudServer.SendGfxLow(game.Pid, low));
             return Task.FromResult(false);
         }
     }
