03df932243f71b11f843a35de5dee08e8844debe
GiR_Zippo <gir_zippo@hellokitty.com>
2024-05-25 17:06:13 +0200

    Update to version 1059

--- BardMusicPlayer.Ui/MainWindow.xaml.cs
+++ BardMusicPlayer.Ui/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace BardMusicPlayer.Ui
         public MainWindow()
         {
             InitializeComponent();
-            this.Title = "LightAmp Ver:" + Assembly.GetExecutingAssembly().GetName().Version + " - Laika";
+            this.Title = "LightAmp Ver:" + Assembly.GetExecutingAssembly().GetName().Version + " - Flatorte";
             if (BmpPigeonhole.Instance.ClassicUi)
                 SwitchClassicStyle();
             else
